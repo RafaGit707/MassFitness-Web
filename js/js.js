5355df5da1e977e834c2e7e11b2749b857625785
RafaGit707 <rgalcal303@g.educaand.es>
2025-03-11 17:41:23 +0100

    Topeheader

---
+++ js/js.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const header = document.querySelector("header");
+    const main = document.querySelector("main");
+    const triggerHeight = window.innerHeight - header.offsetHeight;
+
+    function checkScroll() {
+        if (window.scrollY > triggerHeight) {
+            header.classList.add("header-fixed");
+            main.classList.add("padding-active");
+        } else {
+            header.classList.remove("header-fixed");
+            main.classList.remove("padding-active");
+        }
+    }
+
+    window.addEventListener("scroll", checkScroll);
+});
