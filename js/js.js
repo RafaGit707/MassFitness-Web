1444aaad0851a30b11f2e992542d2247f3dfbcd6
RafaGit707 <rgalcal303@g.educaand.es>
2025-03-14 05:07:27 +0100

    Topeheader hecho y medias query, falta de iniciar sesion

--- js/js.js
+++ js/js.js
@@ -1,16 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
     const header = document.querySelector("header");
-    const main = document.querySelector("main");
-    const triggerHeight = window.innerHeight - header.offsetHeight;
+    const containerHeader = document.querySelector(".container-header");
 
     function checkScroll() {
-        if (window.scrollY > triggerHeight) {
+
+        const headerTop = header.getBoundingClientRect().top;
+        // const initialHeaderTop = containerHeader.offsetTop;
+        // const initialHeaderPosition = containerHeader.offsetTop + containerHeader.offsetHeight;
+        const triggerHeight = window.innerHeight - header.offsetHeight;
+        const scrollPosition = window.scrollY;
+        
+        if (headerTop <= 0) {
             header.classList.add("header-fixed");
-            main.classList.add("padding-active");
-        } else {
+        }
+        if (scrollPosition <= triggerHeight) {
             header.classList.remove("header-fixed");
-            main.classList.remove("padding-active");
         }
     }
 
