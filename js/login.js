14a2b854a9c09497d44149ad0f64223103fe0ec3
RafaGit707 <rgalcal303@g.educaand.es>
2025-03-19 05:06:47 +0100

    Topheader hecho

--- js/login.js
+++ js/login.js
@@ -1,44 +1,69 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    const passwordInput = document.getElementById('password2');
-    const lengthIcon = document.getElementById('length-icon');
-    const lowercaseIcon = document.getElementById('lowercase-icon');
-    const uppercaseIcon = document.getElementById('uppercase-icon');
-    const numberIcon = document.getElementById('number-icon');
-    const specialCharIcon = document.getElementById('special-char-icon');
+  const form = document.querySelector(".form-container");
+  const errorMessage = document.getElementById("error_message");
+
+  const passwordInput = document.getElementById('password2');
+  const lengthIcon = document.getElementById('length-icon');
+  const lowercaseIcon = document.getElementById('lowercase-icon');
+  const uppercaseIcon = document.getElementById('uppercase-icon');
+  const numberIcon = document.getElementById('number-icon');
+  const specialCharIcon = document.getElementById('special-char-icon');
     
-    passwordInput.addEventListener('input', () => {
-      const password = passwordInput.value;
-    
-      if (password.length < 8 || password.length > 16) {
-        lengthIcon.src = 'img/X.png';
-      } else {
-        lengthIcon.src = 'img/tick.png';
+  function validatePassword() {
+    const password = passwordInput.value;
+    let isValid = true;
+  
+    if (password.length < 8 || password.length > 16) {
+      lengthIcon.src = 'img/X.png';
+      isValid = false;
+    } else {
+      lengthIcon.src = 'img/tick.png';
+    }
+  
+    if (!password.match(/[a-z]/)) {
+      lowercaseIcon.src = 'img/X.png';
+      isValid = false;
+    } else {
+      lowercaseIcon.src = 'img/tick.png';
+    }
+  
+    if (!password.match(/[A-Z]/)) {
+      uppercaseIcon.src = 'img/X.png';
+      isValid = false;
+    } else {
+      uppercaseIcon.src = 'img/tick.png';
+    }
+  
+    if (!password.match(/\d/)) {
+      numberIcon.src = 'img/X.png';
+      isValid = false;
+    } else {
+      numberIcon.src = 'img/tick.png';
+    }
+  
+    if (!password.match(/[#$@!%&*?_]/)) {
+      specialCharIcon.src = 'img/X.png';
+      isValid = false;
+    } else {
+      specialCharIcon.src = 'img/tick.png';
+    }
+
+    if (!isValid) {
+        errorMessage.style.display = "block";
+    } else {
+        errorMessage.style.display = "none";
+    }
+
+    return isValid;
+  }
+
+  passwordInput.addEventListener("input", validatePassword);
+
+  form.addEventListener("submit", function (event) {
+      if (!validatePassword()) {
+          event.preventDefault();
       }
-    
-      if (!password.match(/[a-z]/)) {
-        lowercaseIcon.src = 'img/X.png';
-      } else {
-        lowercaseIcon.src = 'img/tick.png';
-      }
-    
-      if (!password.match(/[A-Z]/)) {
-        uppercaseIcon.src = 'img/X.png';
-      } else {
-        uppercaseIcon.src = 'img/tick.png';
-      }
-    
-      if (!password.match(/\d/)) {
-        numberIcon.src = 'img/X.png';
-      } else {
-        numberIcon.src = 'img/tick.png';
-      }
-    
-      if (!password.match(/[#$@!%&*?_]/)) {
-        specialCharIcon.src = 'img/X.png';
-      } else {
-        specialCharIcon.src = 'img/tick.png';
-      }
-    });    
+  });  
 
 });
